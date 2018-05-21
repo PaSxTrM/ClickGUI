d9ee0b0552737e3d5226a84bb0f811b46c0617ad
xTrMYT24 <xtrmyt24@gmail.com>
2018-05-21 10:53:41 +0200

    Code progress & removed useless stuff & your.Client stuff

--- src/me/xtrm/ClickGUI/objects/CPanel.java
+++ src/me/xtrm/ClickGUI/objects/CPanel.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import your.Client.module.Category;
+import your.Client.module.ModuleManager;
 import your.Client.clickgui.ClickGUI;
 
 import me.xtrm.ClickGUI.utils.ColorUtils;
@@ -24,9 +25,7 @@ public class CPanel {
 	
 	public boolean dragging;
 	
-	public CPanel(Category cat, int x, int y, int width, int height, ClickGUI parent) {
-		elements = new ArrayList<MButton>();
-		
+	public CPanel(Category cat, int x, int y, int width, int height, ClickGUI parent) {		
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -37,16 +36,36 @@ public class CPanel {
 	}
 	
 	public void setup() {
+		elements = new ArrayList<MButton>();
 		
+		for(Module m : ModuleManager.getModules()) { // or Client.instance.moduleManager.getModules() (use what you have)
+			if(m.getCategory() == cat) {
+				elements.add(new MButton(m, 88, height, this));
+			}
+		}
 	}
 	
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		Gui.drawRect(x, y, x + width, y + height, ColorUtils.getClickGUIColor().getRGB());
 		
 		String name = Character.toUpperCase(cat.name().charAt(0)) + cat.name().toLowerCase().substring(1);
-		Minecraft.getMinecraft().fr.drawStringWithShadow(name, x + 3, y + 3, 0xFFFFFF);
 		
-		Minecraft.getMinecraft().fr.drawStringWithShadow(extended ? "-" : "+", x + width - 10, y + 3, 0xFFFFFF);
+		Minecraft.getMinecraft().fontRendererObj.drawStringWithShadow(name, x + 3, y + 3, 0xFFFFFF);
+		Minecraft.getMinecraft().fontRendererObj.drawStringWithShadow(extended ? "-" : "+", x + width - 10, y + 3, 0xFFFFFF);
+		
+		updateMBCoords();
+	}
+	
+	/**
+	 * Updates coords for MButtons (TODO: Animation)
+	 */
+	public void updateMBCoords() {
+		int yPos = y + height;
+		for(MButton mb : elements) {
+			mb.x = x + 1;
+			mb.y = yPos;
+			yPos += mb.height;
+		}
 	}
 
 	public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
--- src/me/xtrm/ClickGUI/objects/MButton.java
+++ src/me/xtrm/ClickGUI/objects/MButton.java
@@ -4,23 +4,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import your.Client.module.Module;
-import your.Client.clickgui.ClickGUI;
 
 public class MButton {
 	
-	public int x, y, width, height, anim;
+	public int x, y, width, height;
 	public boolean extended;
 	
 	public ArrayList<EObject> elements;
 	
 	public Module module;
-	public ClickGUI parent;
+
+	public CPanel parent;
 	
-	public MButton(Module module, int x, int y, ClickGUI parent) {
+	public MButton(Module module, int width, int height, CPanel parent) {
 		elements = new ArrayList<EObject>();
 		
-		this.x = x;
-		this.y = y;
+		this.x = 0;
+		this.y = 0;
+		this.width = width;
+		this.height = height;
 		
 		this.module = module;
 		this.parent = parent;
